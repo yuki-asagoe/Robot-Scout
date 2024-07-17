1b850b06e85dabc525b0064a40d6f683e15f81e8
yuki.asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2024-07-17 18:11:24 +0900

    add Limit switch implements

--- firmware/motor_driver/motor_driver.ino
+++ firmware/motor_driver/motor_driver.ino
@@ -5,21 +5,128 @@ enum class DrivingMode{
   Stop=0,Brake=1,Drive=2,ReverseDrive=3,
 };
 
+const int INPUT_PINS_D[]={
+  10,11,12,9,8,13
+};
+// num : 0 ~ 5
+bool system_input_is_on(int num){
+  if(num<0||num>=6){
+    return false;
+  }
+  return digitalRead(INPUT_PINS_D[num]) == HIGH;
+}
+void system_setup_input(int num){
+  if(num<0||num>=6){
+    return;
+  }
+  pinMode(INPUT_PINS_D[num],INPUT_PULLUP);
+}
+int system_get_input_pin(int num){
+  if(num<0||num>=6){
+    return -1;
+  }
+  return INPUT_PINS_D[num];
+}
+
+struct MotorLimitPin{
+  int input_pin_index;
+  LimitType type;
+};
+
+class MotorStopper{
+  private:
+    int limits_length;
+    MotorLimitPin* limits;
+    bool allowed[4]={};
+  public:
+    MotorStopper();
+    MotorStopper(int motorID);
+    bool allow(DrivingMode mode); 
+    void update();
+};
+MotorStopper::MotorStopper(){
+  this->limits_length=0;
+  this->limits=NULL;
+}
+MotorStopper::MotorStopper(int motorID){
+  this->limits_length=0;
+  for(int i=0;i<9;i++){
+    if(MotorLimits[i].motorID==motorID && MotorLimits[i].type!=LimitType::None)this->limits_length++;
+  }
+  this->limits=(MotorLimitPin*)malloc(sizeof(MotorLimitPin)*limits_length);
+  int current;
+  for(int i=0;i<9;i++){
+    if(MotorLimits[i].motorID==motorID && MotorLimits[i].type!=LimitType::None){
+      MotorLimitPin pin={i,MotorLimits[i].type};
+      this->limits[current++]=pin;
+      system_setup_input(i);
+    }
+  }
+}
+bool MotorStopper::allow(DrivingMode mode){
+  switch(mode){
+    case DrivingMode::Stop:
+    case DrivingMode::Brake:
+      return true;
+    case DrivingMode::Drive:
+      return this->allowed[(int)LimitType::Drive] && this->allowed[(int)LimitType::All];
+    case DrivingMode::ReverseDrive:
+      return this->allowed[(int)LimitType::Reverse] && this->allowed[(int)LimitType::All];
+  }
+  return false;
+}
+void MotorStopper::update(){
+  for(int i=0;i<4;i++){
+    this->allowed[i]=true;
+  }
+  for(int i=0;i<this->limits_length;i++){
+    if(system_input_is_on(this->limits[i].input_pin_index)){
+      this->allowed[(int)(this->limits[i].type)]=false;
+    }
+  }
+}
+
 class MotorDriver{
   private: 
     int pin_in1,pin_in2;
+    DrivingMode current_state;
+    MotorStopper stopper;
   public:
-    MotorDriver(int pin_in1,int pin_in2);
+    MotorDriver(int pin_in1,int pin_in2,MotorStopper stopper);
+    bool checkStopper();
+    void updateStopper();
     void changeState(DrivingMode mode);
 };
 
-MotorDriver::MotorDriver(int pin_in1,int pin_in2){
+MotorDriver::MotorDriver(int pin_in1,int pin_in2,MotorStopper stopper){
   pinMode(pin_in1,OUTPUT);
   pinMode(pin_in2,OUTPUT);
   this->pin_in1=pin_in1;
   this->pin_in2=pin_in2;
+  digitalWrite(pin_in1,LOW);
+  digitalWrite(pin_in2,LOW);
+  this->current_state=DrivingMode::Stop;
+  this->stopper=stopper;
+}
+void MotorDriver::updateStopper(){
+  this->stopper.update();
+}
+bool MotorDriver::checkStopper(){
+  if(!(this->stopper.allow(this->current_state))){
+    this->changeState(DrivingMode::Stop);
+    return true;
+  }
+  return false;
 }
 void MotorDriver::changeState(DrivingMode mode){
+  if(!(this->stopper.allow(mode))){
+    this->current_state=DrivingMode::Stop;
+    digitalWrite(pin_in1,LOW);
+    digitalWrite(pin_in2,LOW);
+    return;
+  }
+  
+  this->current_state=mode;
   switch(mode){
     case DrivingMode::Stop:
       digitalWrite(pin_in1,LOW);
@@ -41,11 +148,11 @@ void MotorDriver::changeState(DrivingMode mode){
 }
 
 MotorDriver drivers[]={
-  MotorDriver(7,6),
-  MotorDriver(4,5),
-  MotorDriver(2,3),
-  MotorDriver(A0,A1),
-  MotorDriver(A3,A2)
+  MotorDriver(7,6,MotorStopper(0)),
+  MotorDriver(4,5,MotorStopper(1)),
+  MotorDriver(2,3,MotorStopper(2)),
+  MotorDriver(A0,A1,MotorStopper(3)),
+  MotorDriver(A3,A2,MotorStopper(4))
 };
 
 void onReceive(int length){
@@ -59,7 +166,7 @@ void onReceive(int length){
       Serial.print(id);
       Serial.println(", skipped");
     }
-    DrivingMode mode=(DrivingMode)(data & 0b00000011)
+    DrivingMode mode=(DrivingMode)(data & 0b00000011);
     drivers[id].changeState(mode);
     Serial.print("change status of ");
     Serial.print(id);
@@ -68,7 +175,7 @@ void onReceive(int length){
   }
 }
 void onRequest(void){
-  Serial.println("message receive");
+  Serial.println("I2C Request Received :");
   Wire.write(I2C_ADDRESS);
 }
 
@@ -83,5 +190,17 @@ void setup() {
   Serial.println("Initialization Finished : Ready..>");
 }
 
+long int lastStopperTimeStamp=0;
 void loop() {
+  long int time = millis();
+  long int timePassedFromStopperCheck=lastStopperTimeStamp-time;
+
+  if(timePassedFromStopperCheck>100){
+    bool stopperWorking=false;
+    lastStopperTimeStamp=time;
+    for(int i=0;i<5;i++){
+      drivers[i].updateStopper();
+      stopperWorking|=drivers[i].checkStopper();
+    }
+  }
 }
--- firmware/motor_driver/variables.h
+++ firmware/motor_driver/variables.h
@@ -1,1 +1,19 @@
 const int I2C_ADDRESS=55;
+
+enum class LimitType{
+  None,Drive,Reverse,All
+};
+
+struct MotorLimit{
+  int motorID;
+  LimitType type;
+};
+
+MotorLimit MotorLimits[]={
+  {0,LimitType::Drive},//1
+  {0,LimitType::Reverse},//2
+  {1,LimitType::Drive},//3
+  {1,LimitType::Reverse},//4
+  {2,LimitType::Drive},//5
+  {2,LimitType::Reverse},//6
+};
